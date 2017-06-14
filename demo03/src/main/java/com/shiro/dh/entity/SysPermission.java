5d08428a8aa865708e7ab721298907cd7b0e0ab7
dhjmjava <457122396@qq.com>
2017-06-15 06:40:40 +0800

    权限表修改

--- demo03/src/main/java/com/shiro/dh/entity/SysPermission.java
+++ demo03/src/main/java/com/shiro/dh/entity/SysPermission.java
@@ -14,6 +14,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 
 @Entity
